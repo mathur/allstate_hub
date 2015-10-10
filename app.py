81ace5815335633efb27420bdab2185bce2ab54c
Rohan Mathur <rohanmathur34@gmail.com>
2015-10-09 22:39:34 -0500

    Push a lot of changes I dont feel like remembering i think
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 
+import nest
 from parse_rest.installation import Push
 from parse_rest.connection import register
 import requests
 
 from models import Rules, Sensors
-from settings_local import APPLICATION_ID, REST_API_KEY, MASTER_KEY
+from settings_local import APPLICATION_ID, REST_API_KEY, MASTER_KEY, username, password
 
 app = Flask(__name__)
 
@@ -13,6 +14,9 @@ window = False
 garage = False
 smoke = False
 water = False
+
+prev_nest_mode = None
+prev_nest_mode_garage = None
 
 @app.route('/data')
 def data():
@@ -23,21 +27,21 @@ def data():
     all_rules = Rules.Query.all()
 
     if 'home.window.open' in ret:
-        window_opened(all_sensors)
+        window_opened(all_sensors, all_rules)
     elif 'home.window.closed' in ret:
-        window_closed(all_sensors)
+        window_closed(all_sensors, all_rules)
     elif 'home.garage.open' in ret:
-        garage_opened(all_sensors)
+        garage_opened(all_sensors, all_rules)
     elif 'home.garage.closed' in ret:
-        garage_closed(all_sensors)
+        garage_closed(all_sensors, all_rules)
     elif 'home.smoke.alarm_on' in ret:
-        smoke_on(all_sensors)
+        smoke_on(all_sensors, all_rules)
     elif 'home.smoke.alarm_off' in ret:
-        smoke_off(all_sensors)
+        smoke_off(all_sensors, all_rules)
     elif 'home.water.sensor.alarm_on' in ret:
-        water_on(all_sensors)
+        water_on(all_sensors, all_rules)
     elif 'home.water.sensor.alarm_off' in ret:
-        water_off(all_sensors)
+        water_off(all_sensors, all_rules)
 
     return ret
 
@@ -48,7 +52,13 @@ def window_opened(all_sensors, all_rules):
             sensor.open = True
             sensor.save()
 
-
+    for rule in all_rules:
+        if rule.rule_id == 2 and rule.is_enabled:
+            napi = nest.Nest(username, password)
+            for device in napi.devices:
+                prev_nest_mode = device.mode
+                device.mode = 'off'
+            print 'Nest mode set to off and previous state stored.'
 
 def window_closed(all_sensors, all_rules):
     window = False
@@ -56,6 +66,13 @@ def window_closed(all_sensors, all_rules):
         if sensor.name == 'Window':
             sensor.open = False
             sensor.save()
+
+    for rule in all_rules:
+        if rule.rule_id == 2 and rule.is_enabled:
+            napi = nest.Nest(username, password)
+            for device in napi.devices:
+                device.mode = prev_nest_mode
+            print 'Mode restored to Nest.'
 
 def garage_opened(all_sensors, all_rules):
     garage = True
@@ -65,8 +82,16 @@ def garage_opened(all_sensors, all_rules):
             sensor.save()
 
     for rule in all_rules:
-        if rule.rule_id == 1 and rule.enabled == True:
+        if rule.rule_id == 1 and rule.is_enabled:
             Push.message("You left a window open! Close it to avoid a security risk before leaving.", channels=["Notifications"])
+        elif rule.rule_id == 3 and rule.is_enabled:
+            napi = nest.Nest(username, password)
+            for device in napi.devices:
+                prev_nest_mode_garage = device.mode
+                device.mode = 'off'
+            print 'Nest mode set to off and previous state stored.'
+        elif rule.rule_id == 4 and rule.is_enabled:
+            Push.message("Make sure the alarm system is enabled!", channels=["Notifications"])
 
 def garage_closed(all_sensors, all_rules):
     garage = False
@@ -74,6 +99,13 @@ def garage_closed(all_sensors, all_rules):
         if sensor.name == 'Garage':
             sensor.open = False
             sensor.save()
+
+    for rule in all_rules:
+        if rule.rule_id == 3 and rule.is_enabled:
+            napi = nest.Nest(username, password)
+            for device in napi.devices:
+                device.mode = prev_nest_mode_garage
+            print 'Mode restored to Nest.'
 
 def smoke_on(all_sensors, all_rules):
     smoke = True
--- settings_local.py
+++ settings_local.py
@@ -1,3 +1,9 @@
+# parse
 APPLICATION_ID = "415yXxeqG8igfalXqo75kyOw45s4AbGU2nVbIxhp"
 REST_API_KEY = "MIunMFDUbvQYU15Mn8oOsieWii5CA1VRjtc35JwS"
 MASTER_KEY = "XuVa40YRhdT3z3Zmjp2Nl3CQADoVL3lBruLA5mEF"
+
+# nest
+username = 'rohan@rmathur.com'
+password = 'allstatehub'
+token_cache = '~/.config/nest/cache'
