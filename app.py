3421801d0d619c27f7b29ab59e6252556ebaebdf
Rohan Mathur <rohanmathur34@gmail.com>
2015-10-09 21:04:55 -0500

    Sensors are updating
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- app.py
+++ app.py
@@ -8,28 +8,92 @@ from settings_local import APPLICATION_ID, REST_API_KEY, MASTER_KEY
 
 app = Flask(__name__)
 
-is_window_open = False
+window = False
+garage = False
+smoke = False
+water = False
 
 @app.route('/data')
 def data():
     ret = requests.get('http://allstatehub.cfapps.io/data').content
-    #requests.delete('http://allstatehub.cfapps.io/data')
-
-    if 'home.window.open' in ret:
-        window_opened()
-    if 'home.window.closed' in ret:
-        window_closed()
+    requests.delete('http://allstatehub.cfapps.io/data')
 
     all_sensors = Sensors.Query.all()
-    print all_sensors
+
+    if 'home.window.open' in ret:
+        window_opened(all_sensors)
+    elif 'home.window.closed' in ret:
+        window_closed(all_sensors)
+    elif 'home.garage.open' in ret:
+        garage_opened(all_sensors)
+    elif 'home.garage.closed' in ret:
+        garage_closed(all_sensors)
+    elif 'home.smoke.alarm_on' in ret:
+        smoke_on(all_sensors)
+    elif 'home.smoke.alarm_off' in ret:
+        smoke_off(all_sensors)
+    elif 'home.water.sensor.alarm_on' in ret:
+        water_on(all_sensors)
+    elif 'home.water.sensor.alarm_off' in ret:
+        water_off(all_sensors)
 
     return ret
 
-def window_opened():
-    is_window_open = True
+def window_opened(all_sensors):
+    window = True
+    for sensor in all_sensors:
+        if sensor.name == 'window':
+            sensor.open = True
+            sensor.save()
 
-def window_closed():
-    is_window_open = False
+def window_closed(all_sensors):
+    window = False
+    for sensor in all_sensors:
+        if sensor.name == 'window':
+            sensor.open = False
+            sensor.save()
+
+def garage_opened(all_sensors):
+    garage = True
+    for sensor in all_sensors:
+        if sensor.name == 'garage':
+            sensor.open = True
+            sensor.save()
+
+def garage_closed(all_sensors):
+    garage = False
+    for sensor in all_sensors:
+        if sensor.name == 'garage':
+            sensor.open = False
+            sensor.save()
+
+def smoke_on(all_sensors):
+    smoke = True
+    for sensor in all_sensors:
+        if sensor.name == 'smoke':
+            sensor.open = True
+            sensor.save()
+
+def smoke_off(all_sensors):
+    smoke = False
+    for sensor in all_sensors:
+        if sensor.name == 'smoke':
+            sensor.open = False
+            sensor.save()
+
+def water_on(all_sensors):
+    water = True
+    for sensor in all_sensors:
+        if sensor.name == 'water':
+            sensor.open = True
+            sensor.save()
+
+def water_off(all_sensors):
+    water = False
+    for sensor in all_sensors:
+        if sensor.name == 'water':
+            sensor.open = False
+            sensor.save()
 
 if __name__ == '__main__':
     register(APPLICATION_ID, REST_API_KEY)
