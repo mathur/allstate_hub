f54be9c7bbfa2323956816a901d4bebb897d6410
Rohan Mathur <rohanmathur34@gmail.com>
2015-10-09 20:05:16 -0500

    Connected to our java backend
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import requests
+
 app = Flask(__name__)
 
-@app.route('/api')
+@app.route('/data')
 def hello_world():
-    return 'Hello World!'
+    return requests.get('http://allstatehub.cfapps.io/data').content
 
 if __name__ == '__main__':
     app.run(debug=True)
