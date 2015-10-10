118c9b9a2614aed09716a7260c262452d4cc680e
Rohan Mathur <rohanmathur34@gmail.com>
2015-10-09 20:08:22 -0500

    Time to build experiences :O
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ import requests
 app = Flask(__name__)
 
 @app.route('/data')
-def hello_world():
-    return requests.get('http://allstatehub.cfapps.io/data').content
+def data():
+    ret = requests.get('http://allstatehub.cfapps.io/data').content
+    requests.delete('http://allstatehub.cfapps.io/data')
+
+    return ret
 
 if __name__ == '__main__':
     app.run(debug=True)
