8943946210ff68be3d70e9626a4093296c522abf
Rohan Mathur <rohanmathur34@gmail.com>
2015-10-10 01:36:28 -0500

    New script upgrades
    bye flask
    
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- backup.py
+++ backup.py
@@ -1,6 +1,7 @@
 import datetime
 from threading import Timer
 
+from flask import Flask
 import nest
 from parse_rest.installation import Push
 from parse_rest.connection import register
@@ -9,6 +10,70 @@ import requests
 from models import History, Rules, Sensors
 from sunset import get_sunset
 from settings_local import APPLICATION_ID, REST_API_KEY, MASTER_KEY, username, password
+
+app = Flask(__name__)
+
+window = False
+garage = False
+smoke = False
+water = False
+
+prev_nest_mode = None
+prev_nest_mode_garage = None
+
+@app.route('/data')
+def data():
+    while(True):
+        ret = requests.get('http://allstatehub.cfapps.io/data').content
+        requests.delete('http://allstatehub.cfapps.io/data')
+
+        if ret != '[]':
+            if 'home.window.open' in ret:
+                print 'Window opened'
+            elif 'home.window.closed' in ret:
+                print 'Window closed'
+            elif 'home.garage.open' in ret:
+                print 'Garage door opened'
+            elif 'home.garage.closed' in ret:
+                print 'Garage door closed'
+            elif 'home.smoke.alarm_on' in ret:
+                print 'Smoke alarm on'
+            elif 'home.smoke.alarm_off' in ret:
+                print 'Smoke alarm off'
+            elif 'home.water.sensor.alarm_on' in ret:
+                print 'Flood sensor detecting flooding'
+            elif 'home.water.sensor.alarm_off' in ret:
+                print 'Flood sensor off'
+
+        all_sensors = Sensors.Query.all()
+        all_rules = Rules.Query.all()
+
+        if 'home.window.open' in ret:
+            window_opened(all_sensors, all_rules)
+        elif 'home.window.closed' in ret:
+            window_closed(all_sensors, all_rules)
+        elif 'home.garage.open' in ret:
+            garage_opened(all_sensors, all_rules)
+        elif 'home.garage.closed' in ret:
+            garage_closed(all_sensors, all_rules)
+        elif 'home.smoke.alarm_on' in ret:
+            smoke_on(all_sensors, all_rules)
+        elif 'home.smoke.alarm_off' in ret:
+            smoke_off(all_sensors, all_rules)
+        elif 'home.water.sensor.alarm_on' in ret:
+            water_on(all_sensors, all_rules)
+        elif 'home.water.sensor.alarm_off' in ret:
+            water_off(all_sensors, all_rules)
+
+        for rule in all_rules:
+            if rule.rule_id == 5 and rule.is_enabled:
+                sunset_time = get_sunset()
+                current_time = datetime.datetime.now().time()
+                if sunset_time < current_time:
+                    # turn LEDs on
+                    pass
+
+    return ret
 
 def window_opened(all_sensors, all_rules):
     window = True
@@ -106,62 +171,6 @@ def water_off(all_sensors, all_rules):
             sensor.open = False
             sensor.save()
 
-window = False
-garage = False
-smoke = False
-water = False
-
-prev_nest_mode = None
-prev_nest_mode_garage = None
-
-register(APPLICATION_ID, REST_API_KEY)
-
-while(True):
-    ret = requests.get('http://allstatehub.cfapps.io/data').content
-    requests.delete('http://allstatehub.cfapps.io/data')
-
-    if ret != '[]':
-        if 'home.window.open' in ret:
-            print 'Window opened'
-        elif 'home.window.closed' in ret:
-            print 'Window closed'
-        elif 'home.garage.open' in ret:
-            print 'Garage door opened'
-        elif 'home.garage.closed' in ret:
-            print 'Garage door closed'
-        elif 'home.smoke.alarm_on' in ret:
-            print 'Smoke alarm on'
-        elif 'home.smoke.alarm_off' in ret:
-            print 'Smoke alarm off'
-        elif 'home.water.sensor.alarm_on' in ret:
-            print 'Flood sensor detecting flooding'
-        elif 'home.water.sensor.alarm_off' in ret:
-            print 'Flood sensor off'
-
-    all_sensors = Sensors.Query.all()
-    all_rules = Rules.Query.all()
-
-    if 'home.window.open' in ret:
-        window_opened(all_sensors, all_rules)
-    elif 'home.window.closed' in ret:
-        window_closed(all_sensors, all_rules)
-    elif 'home.garage.open' in ret:
-        garage_opened(all_sensors, all_rules)
-    elif 'home.garage.closed' in ret:
-        garage_closed(all_sensors, all_rules)
-    elif 'home.smoke.alarm_on' in ret:
-        smoke_on(all_sensors, all_rules)
-    elif 'home.smoke.alarm_off' in ret:
-        smoke_off(all_sensors, all_rules)
-    elif 'home.water.sensor.alarm_on' in ret:
-        water_on(all_sensors, all_rules)
-    elif 'home.water.sensor.alarm_off' in ret:
-        water_off(all_sensors, all_rules)
-
-    for rule in all_rules:
-        if rule.rule_id == 5 and rule.is_enabled:
-            sunset_time = get_sunset()
-            current_time = datetime.datetime.now().time()
-            if sunset_time < current_time:
-                # turn LEDs on
-                pass
+if __name__ == '__main__':
+    register(APPLICATION_ID, REST_API_KEY)
+    app.run(debug=True)
